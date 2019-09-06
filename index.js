ee912b7f78f4d7abbeb69a376fb1a3ee2a87b09a
ayerest <irisiseve@gmail.com>
2019-09-06 10:20:28 -0700

    finished generalized map and reduce lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+import { start } from "repl"
+
 // Add your functions here
+
+function map(array, fn) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(fn(array[i]))
+    }
+    return newArray
+}
+
+function reduce(array, fn, startingPoint=false) {
+    let sum;
+    let counter;
+    if (!!startingPoint) {
+        sum = startingPoint;
+        counter = 0;
+    } else {
+        sum = array[0];
+        counter = 1;
+    }
+    while (counter < array.length) {
+        sum = fn(array[counter], sum);
+        counter++;
+    }
+    return sum;
+}
